aa65523b97a737b2804d6a557a8f34b7f3ed5ccc
Rui <manoel.rui@5sdigital.com>
2017-01-23 23:27:47 -0300

    First javascript code

---
+++ C6_LetsRunSomeJavascript/Starter/app.js
@@ -0,0 +1,8 @@
+// Your Javascript Code Goes Here
+
+a = 1;
+b = 2;
+c = a + b;
+console.log(c);
+
+// Note: The 'starter' folders that appear in this course's downloadable source code is here to give you a starting point to try out coding yourself. The 'finished' folder contains the code as it is when we finish the lecture. You can use the 'finished' folder to compare to your own code, or just examine the finished code.
