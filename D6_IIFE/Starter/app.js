37fd0b05575c8675082b2fb816851a1c16650a9d
Rui <manoel.rui@5sdigital.com>
2017-01-25 21:23:40 -0300

    Imediatly invoked function expressions

---
+++ D6_IIFE/Starter/app.js
@@ -0,0 +1,13 @@
+// Your Javascript Code Goes Here
+
+var firstname = 'Jane';
+
+(function (lastname) {
+    var firstname = 'John';
+    console.log(firstname);
+    console.log(lastname);
+})('Doe');
+
+console.log(firstname);
+
+
