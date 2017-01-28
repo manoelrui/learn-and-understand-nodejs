14f8ceda6d2ceb82a11d70710b91eee8af23b559
manoelrui19 <manoelrui19@gmail.com>
2017-01-28 20:27:13 -0300

    Inheriting from event emitter part 3

--- E11_InheritingFromEventEmitterPart3/Starter/app.js
+++ E11_InheritingFromEventEmitterPart3/Starter/app.js
@@ -1,17 +1,4 @@
-var EventEmitter = require('events');
-var util = require('util');
-
-function Greetr() {
-	EventEmitter.call(this);
-	this.greeting = 'Hello world!';
-}
-
-util.inherits(Greetr, EventEmitter);
-
-Greetr.prototype.greet = function(data) {
-	console.log(this.greeting + ': ' + data);
-	this.emit('greet', data);
-}
+var Greetr = require('./greetr');
 
 var greeter1 = new Greetr();
 
