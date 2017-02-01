064ce994441371517704af54dc1ded4adf7f60e4
Rui <manoel.rui@5sdigital.com>
2017-01-31 22:28:16 -0300

    Streams in http response

--- G6_OutputtingHTMLAndTemplates/Starter/app.js
+++ G6_OutputtingHTMLAndTemplates/Starter/app.js
@@ -1,8 +1,11 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function(req, res) {
     
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello world\n');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    var myFile = fs.readFileSync(__dirname + '/index.htm', 'utf8');
+    myFile = myFile.replace('{Message}', 'Hello Full');
+    res.end(myFile);
     
 }).listen(1337, '127.0.0.1');
