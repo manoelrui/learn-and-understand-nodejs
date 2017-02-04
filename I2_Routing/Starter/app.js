71ce3e8b9b34739e1b09f3d7b07a050b43c232a0
manoelrui19 <manoelrui19@gmail.com>
2017-02-03 22:32:31 -0300

    Routing

--- I2_Routing/Starter/app.js
+++ I2_Routing/Starter/app.js
@@ -11,4 +11,8 @@ app.get('/api', function(req, res) {
 	res.json({ firstname: 'John', lastname: 'Doe' });
 });
 
+app.get('/person/:id', function(req, res) {
+    res.send('<html><head></head><body><h1>Person ' + req.params.id +'</h1></body></html>');
+});
+
 app.listen(port);
