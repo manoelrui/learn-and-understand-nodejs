b09c8da3c0019fe57b2a87bf7b46abc59bfffaf3
manoelrui19 <manoelrui19@gmail.com>
2017-02-04 12:44:43 -0300

    Query string and post

--- I5_QuerystringsAndPost/Starter/app.js
+++ I5_QuerystringsAndPost/Starter/app.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var jsonParser = bodyParser.json();
 
 var port = process.env.PORT || 3000;
 
@@ -17,7 +21,20 @@ app.get('/', function(req, res) {
 });
 
 app.get('/person/:id', function(req, res) {
-	res.render('person', { ID: req.params.id });
+	res.render('person', { ID: req.params.id, Qstr: req.query.qstr});
+});
+
+app.post('/person', urlencodedParser, function (req, res) {
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
+    res.send('Thanks');
+});
+
+app.post('/personjson', jsonParser, function (req, res) {
+    res.send('Thank you for json data!!!');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
+
 });
 
 app.get('/api', function(req, res) {
