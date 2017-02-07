d18a46e403b897d58d55f1d35afea61575839980
manoelrui19 <manoelrui19@gmail.com>
2017-02-06 23:45:32 -0300

    Node and mysql

--- J2_NodeAndMySQL/Starter/app.js
+++ J2_NodeAndMySQL/Starter/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
 
 var apiController = require('./controllers/apiController');
 var htmlController = require('./controllers/htmlController');
@@ -12,8 +13,25 @@ app.set('view engine', 'ejs');
 
 app.use('/', function (req, res, next) {
 	console.log('Request Url:' + req.url);
+
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "nodejs_course"
+    });
+
+    con.query('SELECT People.ID, Firstname, Lastname, Address FROM People INNER JOIN PersonAddresses ON People.ID = PersonAddresses.PersonID INNER JOIN Addresses ON PersonAddresses.AddressID = Addresses.ID',
+        function(err, rows) {
+            if(err) throw err;
+            console.log(rows);
+        }
+    );
+
 	next();
 });
+
+
 
 htmlController(app);
 
