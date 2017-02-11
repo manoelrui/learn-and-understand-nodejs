4f780eaaae3e5bf5b689fc70454fab8df621e3e3
manoelrui19 <manoelrui19@gmail.com>
2017-02-10 22:40:46 -0300

    Setting up mongo and mongoose

---
+++ L3_SettingUpMongoAndMongoose/Starter/config/index.js
@@ -0,0 +1,7 @@
+var configValues = require('./config');
+
+module.exports = {
+  getDbConnectionString: function() {
+      return 'mongodb://' + configValues.uname + ':' + configValues.pwd + '@localhost:27017/nodetodosample';
+  }
+};
